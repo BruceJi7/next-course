63adf173e4ef42b30e988174cd80ecb6097e4c98
bruceji7 <revolverc24@gmail.com>
2022-01-18 15:34:29 +0900

    useSWR and client side fetching/hybrid fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,37 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import useSWR from "swr";
 
-export default function LastSalesPage() {
-  const [salesData, setSalesData] = useState(null);
-  const [isLoading, setLoading] = useState(false);
+export default function LastSalesPage(props) {
+  async function fetcher(...args){
+    return fetch(...args).then((res) => res.json())
+    .then((data) => {
+            const sales = [];
+            for (const key in data) {
+              sales.push({
+                id: key,
+                username: data[key].username,
+                volume: data[key].volume,
+              });
+            };
+            return sales
+  })
+}
 
-  useEffect(() => {
-    setLoading(true);
-    fetch("https://nextjs-course-51fab-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const sales = [];
-        for (const key in data) {
-          sales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
+  const { data, error } = useSWR(
+    "https://nextjs-course-51fab-default-rtdb.firebaseio.com/sales.json",
+    (url) => fetcher(url)
+  );
 
-        setSalesData(sales);
-        setLoading(false);
+  if (error) {
+    console.log(`error`, error)
+    return <div>Error loading data</div>;
+  }
+
+  return (
+    <ul>
+      {!data && !error ?   
+        props.sales.map((sale) => (
+          <li key={sale.id}>
+            {sale.username} - {sale.volume} items
+          </li>
+        )):
+        data.map((sale) => (
+          <li key={sale.id}>
+            {sale.username} - {sale.volume} items
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
+export async function getStaticProps(){
+  const response = await fetch("https://nextjs-course-51fab-default-rtdb.firebaseio.com/sales.json");
+  const data = await response.json();
+  const sales = [];
+    for (const key in data) {
+      sales.push({
+        id: key,
+        username: data[key].username,
+        volume: data[key].volume,
       });
-  }, []);
-
-  if (isLoading){
-    return <div>Loading...</div>
-  }
-
-  if (!salesData){
-    return <div>No data</div>
-  }
-
-  return <ul>
-    {salesData.map((sale)=> <li key={sale.id}>{sale.username} - {sale.volume} items</li>)}
-  </ul>;
+    }
+    return {props: {sales: sales}, revalidate:10};
 }
